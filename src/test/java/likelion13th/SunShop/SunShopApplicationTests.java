20299d62370789c27f456e9ece21a031505ea947
Zsun3011 <qkrwltjs3011@naver.com>
2025-05-29 19:57:46 +0900

    message

---
+++ src/test/java/likelion13th/SunShop/SunShopApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.SunShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
